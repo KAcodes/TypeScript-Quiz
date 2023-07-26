15f4c88a15d1d6ebec40f158337afab8a8523612
Kayode Apena <kayodeapena@gmail.com>
2023-07-26 16:47:24 +0100

    fix html entity and button bashing bugs

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -1,57 +1,73 @@
-import React from 'react';
+import React from "react";
 
 type Prop = {
-  question: string,
-  choices: string[],
-  answer: string,
+  question: string;
+  choices: string[];
+  answer: string;
   selectAnswer: (chosen: string) => void;
-}
+};
 
 // Component returns question with optional answers
 // Button click changes colour of buttons and determines if guess is correct
 
-const Question: React.FC<Prop> = ({question, choices, answer, selectAnswer}) => {
+const Question: React.FC<Prop> = ({
+  question,
+  choices,
+  answer,
+  selectAnswer,
+}) => {
 
-const highlightResponse = (guess: string, answ: string) => {
+  const disableBtns = () => {
+    const buttonAnswers = document.querySelectorAll("button");
+  }
 
-  const buttonAnswers = document.querySelectorAll("button");
-    for (let i = 0; i < buttonAnswers.length; i++){
-
-      if (buttonAnswers[i].innerHTML == answ){
+  const highlightResponse = (guess: string, answ: string) => {
+    const buttonAnswers = document.querySelectorAll("button");
+    for (let i = 0; i < buttonAnswers.length; i++) {
+      if (buttonAnswers[i].innerHTML == answ) {
         buttonAnswers[i].classList.add("correctAnswer");
         setTimeout(() => {
-          buttonAnswers[i].classList.remove("correctAnswer")
-        }, 1500);
-      };
-
-      if (buttonAnswers[i].innerHTML == guess && buttonAnswers[i].innerHTML != answ) {
-        buttonAnswers[i].classList.add("incorrectAnswers");
-        setTimeout(() => {
-          buttonAnswers[i].classList.remove("incorrectAnswers")
+          buttonAnswers[i].classList.remove("correctAnswer");
         }, 1500);
       }
+
+      if (
+        buttonAnswers[i].innerHTML == guess && buttonAnswers[i].innerHTML != answ
+      ) {
+        buttonAnswers[i].classList.add("incorrectAnswers");
+        setTimeout(() => {
+          buttonAnswers[i].classList.remove("incorrectAnswers");
+        }, 1500);
+      }
+      
+      buttonAnswers[i].disabled = true;
+      setTimeout(() => {
+        buttonAnswers[i].disabled = false;
+      }, 1500);
+      
     }
-}
-    
+  };
+
   return (
     <div>
-      <h4 id='question'>{question}</h4>
+      <h4 id="question">{question}</h4>
       <div className="answers-list ">
-        { choices.map((choice, index) => (
-          <button 
+        {choices.map((choice, index) => (
+          <button
             key={index}
             className="answers my-2"
             onClick={(e) => {
-              e.preventDefault(); 
-              selectAnswer(choice)
-              highlightResponse(choice, answer)
-          }
-        }
-        >{choice}</button>
-        ))} 
+              e.preventDefault();
+              selectAnswer(choice);
+              highlightResponse(choice, answer);
+            }}
+          >
+            {choice}
+          </button>
+        ))}
       </div>
     </div>
-  )
+  );
 };
 
-export default Question
+export default Question;
--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -1,111 +1,119 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 import _ from "lodash";
-import Question from './question';
+import Question from "./question";
 
-
-
-type NewQuestion = {  
-    category: string,
-    correct_answer: string,
-    difficulty: string,
-    incorrect_answers: string[],
-    question: string,
-    type: string
-  }
+type NewQuestion = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
 
 const loading = [
-    {
-        thisQuestion: "Loading...",
-        possibleAnswers: [""],
-        answer: ""
-    },
+  {
+    thisQuestion: "Loading...",
+    possibleAnswers: [""],
+    answer: "",
+  },
 ];
 
 // Component returns quiz interface with functionality creating dynamic content
 
 const Quiz = () => {
+  const [newQuestions, setNewQuestions] = useState(loading);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [correctCount, setCorrectCount] = useState(0);
+  const [isGameFinished, setIsGameFinished] = useState(false);
+  const [quizLoading, setQuizLoading] = useState(null);
 
-    const [newQuestions, setNewQuestions] = useState(loading);
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [correctCount, setCorrectCount] = useState(0);
-    const [isGameFinished, setIsGameFinished] = useState(false);
+  // async func creates data to be used as questions through fetch API, updates every time quiz is finished
+  useEffect(() => {
+    const APIQuestions = async () => {
+      const response = await fetch("https://opentdb.com/api.php?amount=10");
+      const result = await response.json();
+      const data = await result.results;
 
-    // async func creates data to be used as questions through fetch API, updates every time quiz is finished  
-    useEffect(() => {
+      //function converts html entities from fetch return into strings
+      function decode(str: string) {
+        let txt = new DOMParser().parseFromString(str, "text/html");
+        return txt.documentElement.textContent;
+      }
 
-        const APIQuestions = async() => {
-            const response = await fetch("https://opentdb.com/api.php?amount=10");
-            const result = await response.json();
-            const data = await result.results;
-        
-            //function converts html entities from fetch return into strings 
-            function decode(str: string) {
-                
-                let txt = new DOMParser().parseFromString(str, "text/html");
-                return txt.documentElement.textContent;
-                }
+      //shuffles answers array
+      const myQuizInfo = await data.map((item: NewQuestion) => {
+        const allAnswers: string[] = [
+          ...item.incorrect_answers,
+          item.correct_answer,
+        ];
+        const randomAnswers = _.shuffle(allAnswers).map(question => decode(question));
 
-            //shuffles answers array
-            const myQuizInfo = await data.map((item: NewQuestion) => {
-                const allAnswers: string[] = [...item.incorrect_answers, item.correct_answer];
-                const randomAnswers = _.shuffle(allAnswers);
-                
-                return {
-                        thisQuestion: decode(item.question),
-                        possibleAnswers: randomAnswers,
-                        answer: decode(item.correct_answer)
-                };
-            });
-            setNewQuestions(myQuizInfo);
-        }
-
-        APIQuestions()
-    }, [isGameFinished]);
-    
-   
-    const handleAnswer = (chosenAnswer: string) => {
-        setTimeout(() => {
-            if (chosenAnswer === newQuestions[currentQuestion].answer) {
-                setCorrectCount(correctCount + 1)
-            }
-    
-            const nextQuestion = currentQuestion + 1;  
-            if (nextQuestion < newQuestions.length) {
-                setCurrentQuestion(nextQuestion)
-            }
-            else setIsGameFinished(true);
-        }, 1500);
-        
+        return {
+          thisQuestion: decode(item.question),
+          possibleAnswers: randomAnswers,
+          answer: decode(item.correct_answer),
+        };
+      });
+      setNewQuestions(myQuizInfo);
+      console.log(isGameFinished);
     };
 
-  return (
-    <div className='quiz-container'>
-        <h1>Trivia Quiz</h1>
-        {isGameFinished ? 
-            <div>
-                <p>Well done, you finished the quiz! You scored {correctCount}/10!</p>
-                <button 
-                    onClick={() => {
-                        setCurrentQuestion(0);
-                        setCorrectCount(0);
-                        setIsGameFinished(false);
-                }}>Start Again?</button>
-            </div>
-        : 
-        <div>
-            <h3>Question {currentQuestion + 1}/10</h3>
-            <Question 
-                question={newQuestions[currentQuestion].thisQuestion} 
-                choices={newQuestions[currentQuestion].possibleAnswers}
-                answer={newQuestions[currentQuestion].answer}
-                selectAnswer={handleAnswer}
-            />
-            <h4>Correct Answers: {correctCount}</h4>
-        </div>
-        }
-        
-    </div>
-  )
-}
+    APIQuestions();
+  }, [isGameFinished]);
 
-export default Quiz
+  const handleAnswer = (chosenAnswer: string) => {
+
+    const nextQuestion = currentQuestion + 1;
+    setTimeout(() => {
+      if (chosenAnswer === newQuestions[currentQuestion].answer) {
+        setCorrectCount(correctCount + 1);
+      }
+
+      if (nextQuestion < newQuestions.length) {
+        setCurrentQuestion(nextQuestion);
+      } 
+    }, 1500);
+
+    if (nextQuestion == newQuestions.length) {
+      console.log(newQuestions[0].thisQuestion)
+      setCurrentQuestion(0);
+      setCorrectCount(0);
+      console.log(newQuestions[0].thisQuestion)
+      console.log(currentQuestion)
+      setIsGameFinished(true);
+    }
+  };
+
+  return (
+    <div className="quiz-container">
+      <h1>Trivia Quiz</h1>
+      {isGameFinished ? (
+        <div>
+          <p>Well done, you finished the quiz! You scored {correctCount}/10!</p>
+          <button
+            onClick={() => {
+              setIsGameFinished(false);
+              console.log(currentQuestion)
+            }}
+          >
+            Start Again?
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3>Question {currentQuestion + 1}/10</h3>
+          <Question
+            question={newQuestions[currentQuestion].thisQuestion}
+            choices={newQuestions[currentQuestion].possibleAnswers}
+            answer={newQuestions[currentQuestion].answer}
+            selectAnswer={handleAnswer}
+          />
+          <h4>Correct Answers: {correctCount}</h4>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Quiz;
